5d3583972d409190ba906a23b547c6b291bb4072
topleft <pete.james.jeffryes@gmail.com>
2015-07-11 13:32:09 -0600

    Added html loanTerms input  form, jQuery links input to variable names

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // This program calculates interest paid on a loan given the APR %, total
 // loan amount, and a duration of the lone assuming the same payment amount each
 // month.
-
+//
 // arguments needed:
 //    aprPercentage = float
 //    totalLoanAmount = int
 //    loanDuration = int, time in years
 //    epsilon = int, dollar amount degree of accuracy
 
-
-function loanCalc (aprPercentage, totalLoanAmount, loanDuration, epsilon) {
+function findMonthlyPayment(totalLoanAmount, aprPercentage, loanDuration, epsilon) {
   var monthlyPercentage = aprPercentage/12;
   var months = loanDuration * 12;
   var balance = totalLoanAmount;
@@ -17,13 +16,14 @@ function loanCalc (aprPercentage, totalLoanAmount, loanDuration, epsilon) {
   var hiGuess = ((balance*aprPercentage)+balance)/months;
   var lowGuess = balance/months;
   var monthlyPaymentGuess = (hiGuess+lowGuess)/2;
-  console.log("Hi low Guess: "+lowGuess+","+hiGuess);
+  // console.log("Hi low Guess: "+lowGuess+","+hiGuess);
 
 
   while(Math.abs(balance) >= epsilon) {
-      balance = totalLoanAmount;
-      console.log("Payment Guess: "+monthlyPaymentGuess);
+    // reset balance
+    balance = totalLoanAmount;
 
+    // run through monthly payments for loan duration, recalculating balance each month
     for (var i = 0; i < months; i++) {
       balance += (balance * monthlyPercentage);
       balance -= monthlyPaymentGuess;
@@ -31,38 +31,74 @@ function loanCalc (aprPercentage, totalLoanAmount, loanDuration, epsilon) {
     }
 
     if (Math.abs(balance) <= epsilon) {
-      console.log("Interest Paid: $"+(totalPaid - totalLoanAmount).toFixed(2));
       console.log("Monthly Payment: $"+(monthlyPaymentGuess).toFixed(2));
+      console.log("Total Paid: $"+totalPaid.toFixed(2));
+      console.log("Interest Paid: $"+(totalPaid - totalLoanAmount).toFixed(2));
+      // return an array to access values in jQuery
+      return [(monthlyPaymentGuess).toFixed(2),
+              totalPaid.toFixed(2),
+              (totalPaid - totalLoanAmount).toFixed(2)];
       }
 
 
     else if (balance < epsilon) {
-    console.log(balance);
-    console.log("Guess too high: "+monthlyPaymentGuess);
-    hiGuess = monthlyPaymentGuess;
-    monthlyPaymentGuess = (hiGuess + lowGuess)/2
-    totalPaid = 0;
-      // Checking for stale mate in payment guess calculation
-      if (Math.abs(lowGuess-monthlyPaymentGuess) < 1) {
-        monthlyPaymentGuess -= 100;
-    }
-    }
-    else if (balance > epsilon) {
-    console.log(balance);
-    console.log("Guess too low: "+monthlyPaymentGuess);
-    lowGuess = monthlyPaymentGuess;
-    monthlyPaymentGuess = (hiGuess + lowGuess)/2
-    totalPaid = 0;
-      // Checking for stale mate in payment guess calculation
-      if (Math.abs(lowGuess-monthlyPaymentGuess) < 1) {
-        monthlyPaymentGuess += 100;
+      // console.log(balance);
+      // console.log("Guess too high: "+monthlyPaymentGuess);
+      hiGuess = monthlyPaymentGuess;
+      monthlyPaymentGuess = (hiGuess + lowGuess)/2
+      totalPaid = 0;
+        // Checking for stale mate in payment guess calculation
+        if (Math.abs(lowGuess-monthlyPaymentGuess) < 1) {
+          monthlyPaymentGuess -= 100;
       }
     }
-
-
+    else if (balance > epsilon) {
+      // console.log(balance);
+      // console.log("Guess too low: "+monthlyPaymentGuess);
+      lowGuess = monthlyPaymentGuess;
+      monthlyPaymentGuess = (hiGuess + lowGuess)/2
+      totalPaid = 0;
+        // Checking for stale mate in payment guess calculation
+        if (Math.abs(lowGuess-monthlyPaymentGuess) < 1) {
+          monthlyPaymentGuess += 100;
+        }
     }
-
   }
+};
 
 
-loanCalc (0.07, 16000, 3, 100);
+$(document).ready(function() {
+  $("#loanTerms").on("submit", function(event) {
+    event.preventDefault();
+    var totalLoanAmount = $(this).find('#amountInput').val();
+    var aprPercentage = $(this).find('#aprInput').val();
+    var loanDuration = $(this).find('#timeInput').val();
+    console.log(totalLoanAmount, aprPercentage, loanDuration);
+    // findMonthlyPayment(totalLoanAmount, aprPercentage, loanDuration, 100);
+    // findMonthlyPayment(16000, 0.07, 3, 100);
+    });
+});
+
+// the code below has variable names are that are the same as the one inside
+// the main function, is this going to be bad?
+// $(document).ready(function() {
+//   $("#loanTerms").on("submit", function(event) {
+//     event.preventDefault();
+//     var totalLoanAmount = $(this).find('#amountInput').val();
+//     var aprPercentage = $(this).find('#aprInput').val();
+//     var loanDuration = $(this).find('#timeInput').val();
+//     console.log(totalLoanAmount, aprPercentage, loanDuration);
+//     findMonthlyPayment(totalLoanAmount, aprPercentage, loanDuration, 100);
+//     findMonthlyPayment(16000, 0.07, 3, 100);
+//     });
+// });
+
+// findMonthlyPayment (16000, 0.07, 3, 100);
+
+// return array and loop through array to retreive values
+
+// values needed:
+//    monthly payment amount
+//    total paid
+//    interest paid
+//
